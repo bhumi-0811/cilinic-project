b042f4a0a5e7877cc8903e347f9e140bc566f1ef
Bhumika Thakre <bhumikathakre100@gmail.com>
2026-07-11 21:34:28 +0530

    render connection

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: 'https://cilinic-project.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('vc_admin_token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
   return config
 })
 
